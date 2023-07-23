c0c96e232606c5aa05ec6547c5cdf5a7521a059e
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-07-23 18:03:16 +0200

    #feat Init Spring Boot

---
+++ src/main/java/com/dbprak21/shop_middleware/ShopMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.dbprak21.shop_middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMiddlewareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbprak21/shop_middleware/ShopMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbprak21.shop_middleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
