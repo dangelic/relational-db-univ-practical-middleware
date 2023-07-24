c12bc8091b4d330ad9e88d3fe603eb12b19cbf94
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-07-24 15:18:58 +0200

    #feat Delete the Product Repository

--- src/main/java/uni/dbprak21/shopmiddleware/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package uni.dbprak21.shopmiddleware.repository;
-
-import uni.dbprak21.shopmiddleware.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, String>{
-
-}
