c0aa43e475a27065c20f6c21639e1d61c1fd3900
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-07-25 21:43:07 +0200

    #refactor Add defaults to not have to use abstract interface implementation

--- src/main/java/uni/dbprak21/shopmiddleware/ShopMiddlewareInterface.java
+++ src/main/java/uni/dbprak21/shopmiddleware/ShopMiddlewareInterface.java
@@ -1,31 +1,22 @@
 package uni.dbprak21.shopmiddleware;
 
+import uni.dbprak21.shopmiddleware.model.PriceInfo;
 import uni.dbprak21.shopmiddleware.model.Product;
 
+import java.util.Collections;
 import java.util.List;
 
 public interface ShopMiddlewareInterface {
 
-    // void init(Properties properties);
-
-    // void finish();
-
-    // Product getProduct(String productId);
-
-    List<Product> getProducts(String pattern);
-
-    List<Product> getTopProducts(int k);
-
-    //Category getCategoryTree();
-
-    // List<Product> getProductsByCategoryPath(String categoryPath);
-
-    // List<Product> getSimilarCheaperProduct(String productId);
-
-    // void addNewReview(Review review);
-    // we need a look at all reviews here also...
-
-    // List<User> getTrolls(float rating);
-
-    // List<Offer> getOffers(String productId);
+    // default void init(Properties properties) {};
+    default void finish() {};
+    default Product getProduct(String productId) { return null; };
+    default List<Product> getProducts(String pattern) { return Collections.emptyList(); };
+    default List<Product> getTopProducts(int k) { return Collections.emptyList(); };
+    // default Category getCategoryTree() { return null; };
+    default List<Product> getProductsByCategoryPath(String categoryPath) { return Collections.emptyList(); };
+    default List<Product> getSimilarCheaperProduct(String productId) { return Collections.emptyList(); };
+    // default void addNewReview(Review review) {};
+    // default List<User> getTrolls(float rating) { return Collections.emptyList(); };
+    default List<PriceInfo> getOffers(String productId) { return Collections.emptyList(); };
 }
